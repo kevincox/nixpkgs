bede6bb681bc67baa0a041970b8fe802ea9a7394
Kevin Cox <kevincox@kevincox.ca>
2024-05-01 11:59:56 -0400

    Revert "podman: 4.9.3 -> 5.0.1"
    This reverts commit f5b40ab6b5b3c986a055e18813d0e32219eb6092.

--- pkgs/applications/virtualization/podman/default.nix
+++ pkgs/applications/virtualization/podman/default.nix
@@ -17,12 +17,9 @@
 , makeWrapper
 , runtimeShell
 , symlinkJoin
-, substituteAll
 , extraPackages ? [ ]
 , runc
 , crun
-, gvisor
-, youki
 , conmon
 , slirp4netns
 , fuse-overlayfs
@@ -33,7 +30,6 @@
 , gvproxy
 , aardvark-dns
 , netavark
-, passt
 , testers
 , podman
 }:
@@ -41,6 +37,9 @@ let
   # do not add qemu to this wrapper, store paths get written to the podman vm config and break when GCed
 
   binPath = lib.makeBinPath (lib.optionals stdenv.isLinux [
+    runc
+    crun
+    conmon
     fuse-overlayfs
     util-linux
     iptables
@@ -58,29 +57,23 @@ let
       catatonit # added here for the pause image and also set in `containersConf` for `init_path`
       netavark
       slirp4netns
-      passt
     ];
   };
 in
 buildGoModule rec {
   pname = "podman";
-  version = "5.0.2";
+  version = "4.9.3";
 
   src = fetchFromGitHub {
     owner = "containers";
     repo = "podman";
     rev = "v${version}";
-    hash = "sha256-8Swqwyzu/WI9mG21bLF81Kk4kS2Ltg0GV9G3EcG/FnU=";
+    hash = "sha256-PdAXcXtc/Jl3ttWWB6TciiOwWescJ51Glhf2ZhOw550=";
   };
 
   patches = [
     # we intentionally don't build and install the helper so we shouldn't display messages to users about it
     ./rm-podman-mac-helper-msg.patch
-  ] ++ lib.optionals stdenv.isLinux [
-    (substituteAll {
-      src = ./hardcode-paths.patch;
-      inherit crun runc gvisor youki conmon;
-    })
   ];
 
   vendorHash = null;
